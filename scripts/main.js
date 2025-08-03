71c9dfa793f40503e70e952ddfc875597641ceef
bnelson607 <34819130+bnelson607@users.noreply.github.com>
2025-08-03 18:21:17 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,3 @@
-// // Load header and footer dynamically
-// document.addEventListener("DOMContentLoaded", () => {
-//   fetch("/sports-history-simulations/header.html")
-//     .then(res => res.text())
-//     .then(html => {
-//       document.getElementById("site-header").innerHTML = html;
-//     });
-
-//   fetch("/sports-history-simulations/footer.html")
-//     .then(res => res.text())
-//     .then(html => {
-//       document.getElementById("site-footer").innerHTML = html;
-//     });
-// });
-
-
 // Load header HTML dynamically
 fetch('/sports-history-simulations/header.html')
     .then(response => response.text())
